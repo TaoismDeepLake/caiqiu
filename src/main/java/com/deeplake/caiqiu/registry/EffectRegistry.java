f734d0da66335d7ae2575db9774d9f7bd72f8f98
TaoismDeepLake <pymess@icloud.com>
2023-07-02 19:52:01 +0800

    Update EffectRegistry.java

--- src/main/java/com/deeplake/caiqiu/registry/EffectRegistry.java
+++ src/main/java/com/deeplake/caiqiu/registry/EffectRegistry.java
@@ -1,19 +1,43 @@
 package com.deeplake.caiqiu.registry;
 
+import com.deeplake.caiqiu.IdlFramework;
 import com.deeplake.caiqiu.effects.BaseEffect;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectType;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.IForgeRegistry;
 
+import java.util.function.Supplier;
+
 public class EffectRegistry {
-    public static final Effect B_PURE_WATER = new BaseEffect(EffectType.BENEFICIAL, 0x777733);
-    public static final Effect B_MANTLE = new BaseEffect(EffectType.BENEFICIAL, 0x777733);
+//    public static List<Effect> EFFECT_LIST = new ArrayList<>();
+//    public static final Effect B_PURE_WATER = new BaseEffect(EffectType.BENEFICIAL, 0x777733);
+//    public static final Effect B_MANTLE = new BaseEffect(EffectType.BENEFICIAL, 0x777733);
+    public static final DeferredRegister<Effect> EFFECT_LIST = DeferredRegister.create(ForgeRegistries.POTIONS, IdlFramework.MOD_ID);
+
+    public static final RegistryObject<Effect> DESPAIR_SICKNESS = register("despair", () -> new BaseEffect(EffectType.HARMFUL,0x777733));
+    public static final RegistryObject<Effect> SWAMP_CURSE = register("swamp_curse", () -> new BaseEffect(EffectType.HARMFUL,0x777733));
+    public static final RegistryObject<Effect> DUSK_SYNDROME = register("dusk_syndrome", () -> new BaseEffect(EffectType.HARMFUL,0x777733));
+    public static final RegistryObject<Effect> RECALL_LIGHT = register("recall_light", () -> new BaseEffect(EffectType.HARMFUL,0x777733));
+    public static final RegistryObject<Effect> HOPE_SICKNESS = register("hope_sickness", () -> new BaseEffect(EffectType.HARMFUL,0x777733));
+    public static final RegistryObject<Effect> INCUBATION = register("incubation", () -> new BaseEffect(EffectType.HARMFUL,0x777733));
+
+    public static final RegistryObject<Effect> LOW_SPIRIT = register("low_spirit", () -> new BaseEffect(EffectType.HARMFUL,0x777733));
+    public static final RegistryObject<Effect> ABSENT = register("absent", () -> new BaseEffect(EffectType.HARMFUL,0x777733));
 
     public static void registerAllPotion(RegistryEvent.Register<Effect> event)
     {
         IForgeRegistry<Effect> registry = event.getRegistry();
-        RegistryManager.register(registry, "pure_water", B_PURE_WATER);
-        RegistryManager.register(registry, "mantle", B_MANTLE);
+//        RegistryManager.register(registry, "pure_water", B_PURE_WATER);
+//        RegistryManager.register(registry, "mantle", B_MANTLE);
     }
+
+    public static RegistryObject<Effect> register(final String name, final Supplier<? extends Effect> sup) {
+        RegistryObject<Effect> effectRegistryObject = EFFECT_LIST.register(name, sup);
+        return effectRegistryObject;
+    }
+
 }
